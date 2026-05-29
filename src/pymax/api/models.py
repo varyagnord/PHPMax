d474c5d8e1ef2fbbfdd5f106911dfb8a21d4b678
m-xim <170838360+m-xim@users.noreply.github.com>
2026-05-29 17:19:27 +0300

    feat: update ci

--- src/pymax/api/models.py
+++ src/pymax/api/models.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
 
 
 class CamelModel(BaseModel):
-    model_config = {
-        "alias_generator": to_camel,
-        "populate_by_name": True,
-        "arbitrary_types_allowed": True,
-    }
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        arbitrary_types_allowed=True
+    )
 
     def to_payload(self) -> dict:
         return self.model_dump(by_alias=True, exclude_none=True)
